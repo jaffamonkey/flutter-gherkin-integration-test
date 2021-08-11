89a6f3e03eceffd23c9df10f261e217ec3e1a3e9
pablo <pablo@pablos-mbp.home>
2021-08-12 00:19:30 +0100

    updates

---
+++ run-tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#echo 'Extracting tests.zip...'
+yes | unzip tests.zip
+
+mv application.ipa application.zip
+
+unzip application.zip
+
+mv Payload/Runner.app Runner.app
+
+# make sure build folder exists
+mkdir -p flutter-gherkin-integration-test/build/ios/iphoneos
+
+# move app to build folder
+mv Runner.app flutter-gherkin-integration-test/build/ios/iphoneos/Runner.app
+
+echo 'starting flutter doctor ...'
+flutter-2.2.0 doctor
+
+flutter-2.2.0 devices
+
+cd flutter-gherkin-integration-test
+
+# Install flutter packages
+flutter-2.2.0 pub get
+
+flutter-2.2.0 drive -v --use-application-binary build/ios/iphoneos/Runner.app --driver=test_driver/integration_test_driver.dart --target=integration_test/gherkin_suite_test.dart
